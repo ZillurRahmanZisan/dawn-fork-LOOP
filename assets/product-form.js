35dc2fe5126a8e32f2f497fb18d6ef9fe7ad6f89
ZillurRahmanZisan <zillur@echologyx.com>
2023-06-14 01:57:01 +0600

    Add to cart functionality done

--- assets/product-form.js
+++ assets/product-form.js
@@ -17,6 +17,8 @@ if (!customElements.get('product-form')) {
 
     onSubmitHandler(evt) {
       evt.preventDefault();
+
+      console.log("Hello")
       if (this.submitButton.getAttribute('aria-disabled') === 'true' || this.submitButtonAtc.getAttribute('aria-disabled') === 'true') return;
 
       this.handleErrorMessage();
